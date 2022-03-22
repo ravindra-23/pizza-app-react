9e9874edf9bfd6944c040dd5cc7501d65d5601af
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2022-03-22 16:06:40 +0530

    added banner component

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import AliceCarousel from 'react-alice-carousel'
+import 'react-alice-carousel/lib/alice-carousel.css';
+import './styles.css'
+
+const Banner = () => {
+    const items = [
+        <img src='images/banner1.jpg' alt='spicy pizza' className='banner' />,
+        <img src='images/banner2.jpg' alt='chicken pizza' className='banner' />,
+        <img src='images/banner3.jpg' alt='veg pizza' className='banner' />
+    ]
+
+    const responsive = {
+        0: { items: 1 },
+        568: { items: 1 },
+        1024: { items: 1 },
+    };
+  return (
+      <AliceCarousel 
+        mouseTracking
+        infinite
+        autoPlayInterval={1000}
+        animationDuration={1500}
+        disableDotsControls
+        disableButtonsControls
+        items={items}
+        responsive={responsive}
+        autoPlay
+      />
+  )
+}
+
+export default Banner
