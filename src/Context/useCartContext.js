4d45c5d012a0e14b14379262db8512c71afd0ed0
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2022-03-23 20:38:16 +0530

    added CTA functions

--- src/Context/useCartContext.js
+++ src/Context/useCartContext.js
@@ -5,6 +5,8 @@ const CartContext = createContext();
 const CartContextProvider = ({ children }) => {
     const [cart, setCart] = useState([]);
     console.log(cart)
+    const totalItems = cart.map(item => item.price * item.quantity)
+    const totalPrice = totalItems.reduce((total, item) => total + item, 0).toLocaleString() 
 
     const addToCart = (item) => {
         const existingItem = cart.find(cartItem => cartItem.id === item.id)
@@ -14,8 +16,28 @@ const CartContextProvider = ({ children }) => {
             setCart([...cart, item])
         }
     }
+
+    const removeFromCart = (id) => {
+        setCart(prevCart => prevCart.filter(item => item.id !== id))
+    }
+
+    const emptyCart = () => setCart([])
+
+    const increaseQuantity = (id) => {
+        const updatedCart = cart.map(item => item.id === id ? (
+            {...item, quantity: item.quantity + 1}
+        ) : item)
+        setCart(updatedCart);
+    }
+
+    const decreaseQuantity = (id) => {
+        const updatedCart = cart.map(item => item.id === id ? (
+            {...item, quantity: item.quantity - 1}
+        ) : item)
+        setCart(updatedCart);
+    }
     return (
-        <CartContext.Provider value={{ cart, addToCart }}>
+        <CartContext.Provider value={{ cart, addToCart, removeFromCart, emptyCart, increaseQuantity, decreaseQuantity, totalPrice }}>
             {children}
         </CartContext.Provider>
     )
