de4bff620106ab7a589ddf1b3fe923454a626bb4
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2022-03-23 19:41:22 +0530

    added cart page route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './Pages/Home'
 import './global-styles.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import ProductsPage from './Pages/ProductsPage'
+import CartPage from './Pages/CartPage'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/:param' element={<ProductsPage />} />
+          <Route path='/cart' element={<CartPage />} />
         </Routes>
       </Router>
     </>
