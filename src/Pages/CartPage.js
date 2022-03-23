346fdc63c8c050ca89e40e4a1a1335f0c0a730fb
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2022-03-23 19:38:03 +0530

    added cart page

---
+++ src/Pages/CartPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Navbar, Cart } from '../components'
+
+const CartPage = () => {
+  return (
+    <>
+        <Navbar />
+        <Cart />
+    </>
+  )
+}
+
+export default CartPage
