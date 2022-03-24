3ac5efd1b26f45553e570f4329d189bd632302c5
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2022-03-24 12:26:19 +0530

    added checkout page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './global-styles.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import ProductsPage from './Pages/ProductsPage'
 import CartPage from './Pages/CartPage'
+import CheckoutPage from './Pages/CheckoutPage'
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
           <Route path='/' element={<Home />} />
           <Route path='/:param' element={<ProductsPage />} />
           <Route path='/cart' element={<CartPage />} />
+          <Route path='/checkout/:param' element={<CheckoutPage />} />
         </Routes>
       </Router>
     </>
---
+++ src/Pages/CheckoutPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { CheckoutForm, Navbar } from '../components'
+
+const CheckoutPage = () => {
+  return (
+    <>
+        <Navbar />
+        <CheckoutForm />
+    </>
+  )
+}
+
+export default CheckoutPage
